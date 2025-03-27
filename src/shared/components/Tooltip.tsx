226ec57b64c31b949e49b373122675c53abbaf84
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2025-03-26 22:51:27 -0300

    feat: tooltip show delay

--- src/shared/components/Tooltip.tsx
+++ src/shared/components/Tooltip.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+import { useRef, useState } from "react";
 
 const positions = {
     "top-start": "bottom-full left-0 mb-1",
@@ -16,24 +18,51 @@ interface Props {
     text: string;
     position?: TooltipPosition;
     disabled?: boolean;
+    // 150ms -> better for quickly moving mouse, 500ms -> better for buttons
+    showDelayMs?: number;
     children: React.ReactNode;
 }
 
-export default function Tooltip({ text, position = "top-center", disabled, children }: Props) {
-    if (disabled || !text.trim())
-        return children;
+export default function Tooltip({ text, position = "top-center", disabled, showDelayMs = 500, children }: Props) {
+    const [isVisible, setIsVisible] = useState(false);
+    const timeoutRef = useRef<number>();
+
+    if (disabled || !text.trim()) return children;
+
+    const handleMouseEnter = () => {
+        timeoutRef.current = window.setTimeout(() => {
+            setIsVisible(true);
+        }, showDelayMs);
+    }
+    const handleMouseLeave = () => {
+        setIsVisible(false);
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+            timeoutRef.current = undefined;
+        }
+    }
 
     const positionClass = positions[position] || positions["top-center"];
 
     return (
-        <div className="relative inline-block">
-            <div className="peer">
-                {children}
-            </div>
+        <div
+            className="relative"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+
+            {/* Content */}
+            {children}
 
             {/* Tooltip */}
             <span
-                className={`absolute z-50 invisible peer-hover:visible hover:visible ${positionClass} px-2 py-1 shadow text-sm font-semibold w-max max-w-60 h-fit break-words bg-gray-400 text-gray-800 rounded-lg transition-all duration-200`}
+                className={clsx("absolute z-50  px-2 py-1 shadow text-sm font-semibold w-max max-w-60 h-fit break-words bg-gray-400 text-gray-800 rounded-lg",
+                    positionClass,
+                    {
+                        "invisible": !isVisible,
+                        "visible": isVisible
+                    }
+                )}
                 children={text}
             />
         </div>
