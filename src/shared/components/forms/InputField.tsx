76fdf58f58527c2826f243d942208df39988b341
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2025-08-13 20:28:28 -0300

    feat: add InputField component

---
+++ src/shared/components/forms/InputField.tsx
@@ -0,0 +1,26 @@
+import clsx, { ClassValue } from "clsx";
+
+interface Props {
+    value: string;
+    onChange: (newValue: string) => void;
+    disabled?: boolean;
+    placeholder?: string;
+    className?: ClassValue;
+}
+
+export default function InputField({ value, onChange, disabled, placeholder, className }: Props) {
+    return (
+        <input
+            className={clsx("py-1 px-2 rounded-lg border-2 border-gray-700 outline-none",
+                "transition-colors bg-transparent font-semibold text-gray-500 placeholder:text-gray-500/50",
+                "focus:border-gray-500 focus:text-blue-300 focus:shadow", // <- focus
+                { "hover:border-gray-500 hover:text-blue-300 hover:shadow": !disabled }, // <- hover
+                { "opacity-50 hover:text-blue-300": disabled },
+                className,
+            )}
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+            placeholder={placeholder}
+        />
+    );
+}
