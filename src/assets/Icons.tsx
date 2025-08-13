9c1076fac98dba309ffcdd5523fbd07b19934d4d
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2025-08-13 20:28:35 -0300

    feat: add ExportIcon and ImportIcon components

--- src/assets/Icons.tsx
+++ src/assets/Icons.tsx
@@ -262,3 +262,18 @@ export const UndoIcon = ({ className }: Props) => (
         <path d="M4 9h10.5a5.5 5.5 0 0 1 5.5 5.5a5.5 5.5 0 0 1-5.5 5.5H11" />
     </svg>
 );
+
+
+export const ExportIcon = ({ className }: Props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className={"self-center p-0.5 rounded-full " + className}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+    </svg>
+);
+
+export const ImportIcon = ({ className }: Props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={"self-center p-0.5 rounded-full " + className}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+    </svg>
+
+);
+
